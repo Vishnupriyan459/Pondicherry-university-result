c7e4f8a3ac61bf167f8d69ef2f467915947f5f81
Vishnupriyan459 <vpriyan459@gmail.com>
2024-08-24 10:26:43 +0530

    finished

--- Tool.py
+++ Tool.py
@@ -4,50 +4,8 @@ from openpyxl import Workbook, load_workbook
 import tkinter as tk
 from tkinter import filedialog
 import datetime
-# import open
-
-
-import ctypes
-import time
-# MessageBox types
-MB_OK = 0x0  # OK button
-MB_OKCANCEL = 0x1  # OK and Cancel buttons
-MB_YESNO = 0x4  # Yes and No buttons
-MB_ICONINFORMATION = 0x40  # Information icon
-MB_ICONWARNING = 0x30  # Warning icon
-MB_ICONERROR = 0x10  # Error icon
-
-# Function to display message box
-def show_loading_alert(message):
-    ctypes.windll.user32.MessageBoxW(0, message, "Loading", MB_OK )
-
-# Function to display message box
-def Finish_alert(message):
-    ctypes.windll.user32.MessageBoxW(0, f"Successfully data stored in {message}", "Finished", MB_OK )
-
-
-
-
-
-
-
-
-# Function to display message box
-def show_message_box(title, message, style):
-    return ctypes.windll.user32.MessageBoxW(0, message, title, style)
-
-def show():
-    result = show_message_box("Alert", "would you want to execute this file?", MB_OK | MB_ICONINFORMATION |MB_YESNO)
-    if result == 7:  # 7 corresponds to No button
-        show_loading_alert('Do you want to close?')
-        exit()
-        
-def load():
-    # Simulate some loading process
-    show_loading_alert("Loading, please wait...")
-    time.sleep(1)  # Simulating loading process
-    
-
+import open
+import os
 
 # Create a new workbook
 wb = Workbook()
@@ -123,7 +81,7 @@ headers = {
 
 
 ###create a Alert to run this program
-show()
+open.show()
 
 
 
@@ -177,7 +135,7 @@ first_table = True
 error_reg_numbers = []
 
 ##loading alert
-load()
+open.load()
 
 # Main function to process requests for registration numbers
 for reg_no in register_list:
@@ -254,4 +212,5 @@ for reg_no in error_reg_numbers:
 # Save the workbook
 filename=f"example_sorted_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.xlsx"
 wb.save(filename)
-Finish_alert(filename)
+open.Finish_alert(filename)
+os.startfile(filename)
